e5a4a4a5734f3b20281641df39e515040826dde0
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-04 17:57:02 +0530

    this is basically revison day 1

---
+++ index.js
@@ -0,0 +1,24 @@
+// let fs=require("fs");
+// const content = fs.readFileSync("a.txt","utf-8");
+// console.log(content);
+
+
+
+function sum(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function mult(a,b){
+    return a*b;
+}
+function div(a,b){
+    return a/b;
+}
+//functional argumnets 
+function doop(a,b,op){
+    return op(a,b)
+}
+console.log(doop(5,5,sum));
+
