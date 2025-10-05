17146d7224f57b64a661f8345306c127efe5d4bd
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-05 09:39:12 +0530

    added

--- index.js
+++ index.js
@@ -4,21 +4,27 @@
 
 
 
-function sum(a,b){
-    return a+b;
-}
-function sub(a,b){
-    return a-b;
-}
-function mult(a,b){
-    return a*b;
-}
-function div(a,b){
-    return a/b;
-}
-//functional argumnets 
-function doop(a,b,op){
-    return op(a,b)
-}
-console.log(doop(5,5,sum));
+// function sum(a,b){
+//     return a+b;
+// }
+// function sub(a,b){
+//     return a-b;
+// }
+// function mult(a,b){
+//     return a*b;
+// }
+// function div(a,b){
+//     return a/b;
+// }
+// //functional argumnets 
+// function doop(a,b,op){
+//     return op(a,b)
+// }
+// console.log(doop(5,5,sum));
+
+
+
+// -------------------------
+// document.querySelector("p").innerHTML="Mahesh Kaise ho ";
+
 
