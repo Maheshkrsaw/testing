e55baedd4cac64ff77488c16c10d94c6bdeeda0c
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-07 18:53:14 +0530

    ha ha ha

--- index.js
+++ index.js
@@ -62,15 +62,20 @@
 
 
 //find the no of negative no 
-let arr=[5,10,15,20,25];
-let count=0;
-for(let i=0 ; i<arr.length;i++){
-    if(arr[i]<0){
-        count++;
-    }
-}
+// let arr=[ -5,-10,-15,-20,-25];
+// let count=0;
+// for(let i=0 ; i<arr.length;i++){
+//     if(arr[i]<0){
+//         count++;
+//     }
+// }
 
 
-console.log(count);
+// console.log(count);
 
+
+
+
+
+// write an functon that rewturm the largest number in array 
 
